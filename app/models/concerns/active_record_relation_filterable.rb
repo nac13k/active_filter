15ad9699006bd3c87cb98dfdf7e27630971e4978
Porfirio Diaz <porfirio.diaz@interfactura.com>
2021-12-14 18:38:39 -0600

    fix: remove last commit

--- app/models/concerns/active_record_relation_filterable.rb
+++ app/models/concerns/active_record_relation_filterable.rb
@@ -27,7 +27,7 @@ module ActiveRecordRelationFilterable
       new_search_params = {}
       params.each do |k, v|
         if k.to_s.include?('range')
-           range_params[k] = v.compact
+           range_params[k] = v
         elsif k.to_s.match(/(text)/)
           new_search_params[k] = v
         else
